818963effdee90ea955cca00cf35d9fbe162e1e6
Davysongs <Davidgsongs@gmail.com>
2024-02-07 19:52:44 +0100

    fix page bug

--- RawApp/urls.py
+++ RawApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign/', views.register, name= "register"),
+    path('signup/', views.register, name= "register"),
     path('login/', views.signin, name='login'),
     path('logout/', views.userlogout, name='logout'),
     path('', views.main, name="main"),
