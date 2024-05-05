7561cb4d43eceea320190606f1182d40cc505168
Godson David <davidgsongs@gmail.com>
2024-05-05 17:37:26 +0100

    Update settings.py

--- Quizify/settings.py
+++ Quizify/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vy&)fx3&#3w%=@7dya^ifki-d)&$4t#d%pdfpfoj22soar^1-j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,30 +74,16 @@ WSGI_APPLICATION = 'Quizify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'quizify',
-#         'USER': 'root',
-#         'PASSWORD': '#$(tVA0nr)',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'railway',
-       'USER':'postgres',
-       'PASSWORD': 'JBZCdJGEGkKhMcOyCGQNPmWXjcekZCsM',
-       'HOST':'viaduct.proxy.rlwy.net',
-       'PORT':'37630',
-
+       'ENGINE': os.getenv('DATABASE_ENGINE'),
+       'NAME': os.getenv('DATABASE_NAME'),
+       'USER':os.getenv('DATABASE_USER'),
+       'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+       'HOST':os.getenv('DATABASE_HOST'),
+       'PORT':os.getenv('DATABASE_PORT'),
    }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
