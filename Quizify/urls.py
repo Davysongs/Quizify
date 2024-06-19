6a6de0da628f52144aa5a40cc5ffc56ea295464d
davysongs <davidgsongs@gmail.com>
2024-06-19 16:33:51 +0100

    Structure update

--- Quizify/urls.py
+++ Quizify/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('RawApp.urls')),
+    path('', include('base.urls')),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- Results/apps.py
+++ Results/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ResultsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Results'
+    name = 'results'
